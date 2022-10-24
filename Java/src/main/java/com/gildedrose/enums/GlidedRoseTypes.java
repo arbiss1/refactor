07b01114a3a328e68de539ab310cb7f52c5dda55
arbis.malasi <a.malasi.ext@teamsystem.com>
2022-10-24 12:57:35 +0200

    fix: change some logic parts

--- Java/src/main/java/com/gildedrose/enums/GlidedRoseTypes.java
+++ Java/src/main/java/com/gildedrose/enums/GlidedRoseTypes.java
@@ -4,7 +4,7 @@ public enum GlidedRoseTypes {
     SULFURAS("Sulfuras"),
     BRIE("Aged Brie"),
     CONJURED_ITEM("Conjured"),
-    CONCERT_ITEM("Concret");
+    BACKSTAGE_ITEM("Backstage passes");
 
     public final String value;
 
--- Java/src/main/java/com/gildedrose/service/QualityService.java
+++ Java/src/main/java/com/gildedrose/service/QualityService.java
@@ -9,13 +9,13 @@ public class QualityService {
     private final int QUALITY_MAX_NORMAL_ITEM_VALUE = 50;
 
     public void updateQuality(Item item) {
-        //check days and increase quality
-        //TODO - not sure about this, didnt understand it well
-//        qualityByDays(item);
+        //check days and increase quality, only for backstage
+        qualityByDays(item);
         if (item.name.equals(GlidedRoseTypes.BRIE.value)
-            || item.name.equals(GlidedRoseTypes.CONCERT_ITEM.value)) {
+            || item.name.equals(GlidedRoseTypes.BACKSTAGE_ITEM.value)) {
             item.quality += 2;
         } else {
+            //this includes conjured items
             item.quality -= 2;
         }
     }
@@ -27,15 +27,23 @@ public class QualityService {
         return QUALITY_MAX_NORMAL_ITEM_VALUE;
     }
 
-//    public void qualityByDays(Item item){
-//        if (item.sellIn < 10) {
-//            increaseQuality(2, item);
-//        } else if(item.sellIn < 5){
-//            increaseQuality(3, item);
-//        }
-//    }
+    public void qualityByDays(Item item) {
+        if (checkIfItemIsBackstage(item)) {
+            if (item.sellIn < 10) {
+                increaseQuality(2, item);
+            } else if (item.sellIn < 5) {
+                increaseQuality(3, item);
+            } else if(item.sellIn == 0){
+                item.quality = 0;
+            }
+        }
+    }
 
     public void increaseQuality(int quality, Item item) {
         item.quality += quality;
+    }
+
+    public boolean checkIfItemIsBackstage(Item item){
+        return item.name.equals(GlidedRoseTypes.BACKSTAGE_ITEM.value);
     }
 }
